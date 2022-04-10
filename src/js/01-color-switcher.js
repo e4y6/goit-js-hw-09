d7a11df81cb0bbed82ff1bc9d2c067a23cb615ee
e4y6 <99596168+e4y6@users.noreply.github.com>
2022-04-10 23:38:04 +0300

    color-switcher updated

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startButtonRef = document.querySelector('button[data-start]');
+const stopButtonRef = document.querySelector('button[data-stop]');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+stopButtonRef.disabled = true;
+startButtonRef.addEventListener('click', onStart);
+stopButtonRef.addEventListener('click', onStop);
+
+function onStart() {
+  startButtonRef.disabled = true;
+  stopButtonRef.disabled = false;
+  document.body.style.backgroundColor = getRandomHexColor();
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+function onStop() {
+  stopButtonRef.disabled = true;
+  startButtonRef.disabled = false;
+  clearInterval(timerId);
+}
